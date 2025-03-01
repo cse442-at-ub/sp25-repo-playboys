e7713f3b7b761481d7f799d31362fabfe2598f52
ricoc236 <ricochen23166@gmail.com>
2025-02-28 21:25:25 -0500

    Updated code structure to avoid merging conflict (attempt 2)

---
+++ backend/database.php
@@ -0,0 +1,19 @@
+<?php
+//database connection information for phpadmin sql database
+//will need to be changed when connecting to live server
+$host = "localhost";
+$username = "";
+$password = "";
+$database = "user_login_information"; //sql database name
+
+$port = 3306; //port number for sql database
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $database, $port);
+
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
---
+++ backend/headers.php
@@ -0,0 +1,14 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+    header("Content-Type: application/json");
+    
+    $method = $_SERVER["REQUEST_METHOD"]; // e.g. "POST"
+    // Handle preflight (OPTIONS) requests
+    if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+            http_response_code(200);
+            exit();
+    }
+    require __DIR__ . "/database.php";
+?>
